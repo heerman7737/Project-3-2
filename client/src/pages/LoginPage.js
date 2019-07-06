19ad5209bcfc1a0cdce713a1ed0ce8826b0f4bb3
Giang <giang.to.7737@gmail.com>
2019-07-06 09:28:21 -0700

    change

---
+++ client/src/pages/LoginPage.js
@@ -0,0 +1,32 @@
+// import React from 'react';
+
+// const Auth = props => {
+
+//   return (
+//     <div className="dialog-container">
+//       <div className="dialog">
+//         <form className="dialog-form" onSubmit={connectToChatkit}>
+//           <label className="username-label" htmlFor="username">
+//             Login with your username
+//           </label>
+//           <input
+//             id="username"
+//             className="username-input"
+//             autoFocus
+//             type="text"
+//             name="userId"
+//             value={userId}
+//             onChange={handleInput}
+//             placeholder="Enter your username"
+//           />
+//           <button type="submit" className="submit-btn">
+//             Submit
+//           </button>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// };
+
+
+// export default Auth;
